3e60563f6ce44cf1d1e6f7036bb83b4760193157
Weronika <weronikatatarek@interia.pl>
2024-12-18 09:27:29 +0100

    student-watek

--- stare_rozwiazanie.py
+++ stare_rozwiazanie.py
@@ -7,25 +7,26 @@ from enum import Enum
 from queue import Queue
 from typing import List, Any
 
-choose_waiting_time = 0.1
-
-#studenci watkami
- # 5%
+choose_waiting_time = 0.0000000001
+ 
 class MessageTypes(Enum):
     END = 0,
     STUDENT = 1,
     STUDENT_GETTING_QUESTIONS = 2,
     STUDENT_ANSWERING = 3,
     STUDENT_GRADE = 4,
-    EWAKUACJA = 5,
-    FIELD = 6,
-    STUDENT_ALL_QUESTIONS = 7
+    EWAKUACJA = 5
+
 
 
 @dataclass
 class Msg:
     type: MessageTypes
     data: Any = None
+
+
+GETTING_QUESTIONS = 'getting_questions'
+ANSWERING = "answering"
 
 
 class StudyField:
@@ -54,288 +55,41 @@ class StudentGrade:
     is_theory: bool
 
 
-student_id_to_queue = {}
-
-def broadcast_to_all_students(msg):
-    for q in student_id_to_queue.values():
-        q.put(msg)
-
 def generate_students(num,k):
     students = []
     for student_id in range(1, num + 1):
-        queue = Queue()
         passed_practical = bool(random.random() < 0.05)
         field = random.randint(1,k)
         student = Student(student_id, passed_practical, field)
         students.append(student)
-        student_id_to_queue[student_id] = queue
-        
     return students
 
 
-class Commission(threading.Thread):
-    def __init__(self, semaphore, input_queue, output_queue, dean_queue, evacuation_event,ending_event, theoretical):
+class StudentManager(threading.Thread):
+    def __init__(self, students, before_building_queue, evacuation_event, ending_event):
         super().__init__()
-
-        self.semaphore = semaphore
-        self.input_queue = input_queue
-        self.output_queue = output_queue
-        self.dean_queue = dean_queue
-        self.thinking_queue = Queue()
-        self.evacuation_event = evacuation_event
+        self.students = list(students)
+        random.shuffle(self.students)
+        self.before_building_queue = before_building_queue
+        self.evacuation_event =evacuation_event
         self.ending_event=ending_event
-        self.theoretical = theoretical
         self._stop_event = threading.Event()
 
-        self.member_queues = [Queue() for _ in range(3)]
-        self.thinking_spaces = [
-            ThinkingSpace(self.thinking_queue, self.member_queues[0],self.evacuation_event,self.ending_event, thread_id=i, name=str(theoretical) + str(i)) for i
-            in range(3)
-        ]
-
-        def handle_questions_answers(msg: Msg) -> StudentData:
-            if msg is None or msg.data is None:
-                return None
-            if msg.type == MessageTypes.STUDENT_GETTING_QUESTIONS:
-                data = msg.data
-                time.sleep(choose_waiting_time)
-                questions = data.questions + (random.randint(0, 999),)
-                data = copy.copy(data)
-                data.questions = questions
-                return data
-            elif msg.type == MessageTypes.STUDENT_ANSWERING:
-                data = msg.data
-                time.sleep(choose_waiting_time)
-                # TODO ma miec 95% na zdanie
-                #grade = random.choice([2.0, 3.0, 3.5, 4.0, 4.5, 5.0])
-                grades_list = [2.0, 3.0, 3.5, 4.0, 4.5, 5.0]
-                weights = [0.05, 0.19, 0.19, 0.19, 0.19, 0.19]
-
-                grade = random.choices(grades_list, weights=weights, k=1)[0]
-                grades = data.grades + (grade,)
-                data = copy.copy(data)
-                data.grades = grades
-                return data
-            else:
-                return None
-
-        def handle_student(msg: Msg):
-            new_data = handle_questions_answers(msg)
-            msg.data = new_data
-            key = new_data.student.student_id
-            student_id_to_queue[key].put(msg)
-
-        # def handle1(msg: Msg):
-        #     new_data = handle_questions_answers(msg)
-        #     msg.data = new_data
-        #     self.member_queues[1].put(msg)
-            
-        # def handle2(msg: Msg):
-        #     new_data = handle_questions_answers(msg)
-        #     msg.data = new_data
-        #     self.member_queues[2].put(msg)
-           
-        def handle_last(msg: Msg):
-            new_data = handle_questions_answers(msg)
-            if msg.type == MessageTypes.STUDENT_GETTING_QUESTIONS:
-                key = new_data.student.student_id
-                student_id_to_queue[key].put(
-                    Msg(
-                        MessageTypes.STUDENT_ALL_QUESTIONS,
-                        new_data
-                    )
-                )
-                # self.thinking_queue.put(
-                #     Msg(
-                #         MessageTypes.STUDENT,
-                #         new_data
-                #     )
-                # )
-            elif msg.type == MessageTypes.STUDENT_ANSWERING:
-                student = msg.data.student
-                avg = sum(new_data.grades) / 3
-                final_grade = (
-                    2.0 if avg < 2.5 else
-                    3.0 if avg < 3.25 else
-                    3.5 if avg < 3.75 else
-                    4.0 if avg < 4.25 else
-                    4.5 if avg < 4.75 else
-                    5.0
-                )
-                print(f"Student '{student.student_id}' final grade: {final_grade} from exam : {'Theoretical' if self.theoretical else 'Practical'}")
-
-                self.dean_queue.put(
-                    Msg(
-                        MessageTypes.STUDENT_GRADE,
-                        StudentGrade(student, final_grade, self.theoretical)
-                    )
-                )
-                if final_grade != 2.0:
-                    key = new_data.student.student_id
-                    student_id_to_queue[key].put(
-                        Msg(
-                            MessageTypes.STUDENT_GRADE,
-                            StudentGrade(student, final_grade, self.theoretical)
-                        )
-                    )
-                    
-                self.semaphore.release()
-
-        # Tworzenie członków komisji
-        self.members = [
-            CommisionMember(self.member_queues[0], handle_student,self.evacuation_event,self.ending_event),
-            CommisionMember(self.member_queues[1], handle_student,self.evacuation_event,self.ending_event),
-            CommisionMember(self.member_queues[2], handle_last,self.evacuation_event,self.ending_event),
-        ]
-
-
 
     def run(self):
-        print(f"{'Theoretical' if self.theoretical else 'Practical'} commission -running")
-        for member in self.members:
-            member.start()
-        for space in self.thinking_spaces:
-            space.start()
-        print(f"{'Theoretical' if self.theoretical else 'Practical'} commission -running")
-        msg = None
-        while True:
-            #print(f" Komisja - running")
-            if self.evacuation_event.is_set():
-                print(f"{'Theoretical' if self.theoretical else 'Practical'} commission received evacuation signal.")
+
+        for student in self.students:
+            self.before_building_queue.put(Msg(MessageTypes.STUDENT, StudentData(student)))
+            print(f"Student {student.student_id} comes to exam (is waiting before building) ")
+            time.sleep(random.uniform(0, choose_waiting_time))
+            if self.evacuation_event.is_set() :
+                print(f"student manager received evacuation signal.")
+                self._stop_event.set()
                 break
             if self.ending_event.is_set():
-                print(f"{'Theoretical' if self.theoretical else 'Practical'} commission received ending signal.")
+                print(f"student manager received ending signal.")
+                self._stop_event.set()
                 break
-
-        
-            if not msg:
-                try:
-                    msg = self.input_queue.get(timeout=1)
-                except:
-                    continue
-
-            if not self.semaphore.acquire(timeout=1):
-                continue
-
-            self.member_queues[0].put(
-                Msg(
-                    MessageTypes.STUDENT_GETTING_QUESTIONS,
-                    msg.data
-                )
-            )
-            msg = None
-
-        for member in self.members:
-            member.join()
-        for space in self.thinking_spaces:
-            space.join()
-        print(f"{'Theoretical' if self.theoretical else 'Practical'} commission finished.")
-
-
-
-class StudentRunner(threading.Thread):
-
-    def __init__(self, student: Student, practical_commission: Commission, theoretical_commission: Commission, evacuation_event: threading.Event):
-        super().__init__()
-        self.student = student
-        self.practical_commission = practical_commission
-        self.evacuation_event = evacuation_event
-        self.queue = student_id_to_queue[student.student_id]
-        assert self.queue is not None
-
-    def run(self):
-        # po losowym czasie idzie na egzamin
-        
-        # time.sleep(random.uniform(0, 5 * choose_waiting_time))
-        time.sleep(1)
-
-        field = None
-
-        def _await_msg_type(type: MessageTypes):
-            while True:
-                if self.evacuation_event.is_set():
-                    raise Exception("Ewakuacja")
-                msg: Msg = self.queue.get()
-                if msg.type == type:
-                    return msg
-                
-        msg = _await_msg_type(MessageTypes.FIELD)
-        field = msg.data
-
-        if self.student.field != field:
-            print(f"Student {self.student.student_id}: Ide do domu, nie moj kierunek")
-            return
-        
-        print(f"Student {self.student.student_id}: Ide do komisji praktzycznej")
-        
-        # TODO skip komisji jak ma zdane praktyczne
-
-        # TODO acquire w petli z timeoutem, jak byla ewakuacja
-        while True:
-            if self.evacuation_event.is_set():
-                raise Exception("Ewakucja")
-            if not self.practical_commission.semaphore.acquire(timeout=1):
-                continue
-            else:
-                break
-        
-        self.practical_commission.member_queues[0].put(
-            Msg(
-                MessageTypes.STUDENT_GETTING_QUESTIONS,
-                data=StudentData(self.student, tuple(), tuple())
-            )
-        )
-        
-        msg = _await_msg_type(MessageTypes.STUDENT_GETTING_QUESTIONS)
-        print(msg)
-
-        self.practical_commission.member_queues[1].put(msg)
-        msg = _await_msg_type(MessageTypes.STUDENT_GETTING_QUESTIONS)
-        print(msg)
-
-
-        self.practical_commission.member_queues[2].put(msg)
-        msg = _await_msg_type(MessageTypes.STUDENT_ALL_QUESTIONS)
-        print(msg)
-
-        print(f"Studnet mysli nad odpowiedziami {self.student.student_id}")
-        time.sleep(3)
-
-        self.practical_commission.member_queues[0].put(
-            Msg(
-                MessageTypes.STUDENT_ANSWERING,
-                msg.data
-            )
-        )
-        msg = _await_msg_type(MessageTypes.STUDENT_ANSWERING)
-        print(msg)
-
-        self.practical_commission.member_queues[1].put(
-            Msg(
-                MessageTypes.STUDENT_ANSWERING,
-                msg.data
-            )
-        )
-        msg = _await_msg_type(MessageTypes.STUDENT_ANSWERING)
-        print(msg)
-
-        self.practical_commission.member_queues[2].put(
-            Msg(
-                MessageTypes.STUDENT_ANSWERING,
-                msg.data
-            )
-        )
-        msg = _await_msg_type(MessageTypes.STUDENT_GRADE)
-        print(msg)
-
-        # TODO jak zdal to tak samo z druga komisja
-
-        self.practical_commission.semaphore.release()
-
-
-
-
-
  
 
 
@@ -411,6 +165,145 @@ class ThinkingSpace(threading.Thread):
                 continue
 
 
+class Commission(threading.Thread):
+    def __init__(self, semaphore, input_queue, output_queue, dean_queue, evacuation_event,ending_event, theoretical):
+        super().__init__()
+
+        self.semaphore = semaphore
+        self.input_queue = input_queue
+        self.output_queue = output_queue
+        self.dean_queue = dean_queue
+        self.thinking_queue = Queue()
+        self.evacuation_event = evacuation_event
+        self.ending_event=ending_event
+        self.theoretical = theoretical
+        self._stop_event = threading.Event()
+
+        self.member_queues = [Queue() for _ in range(3)]
+        self.thinking_spaces = [
+            ThinkingSpace(self.thinking_queue, self.member_queues[0],self.evacuation_event,self.ending_event, thread_id=i, name=str(theoretical) + str(i)) for i
+            in range(3)
+        ]
+
+        def handle_questions_answers(msg: Msg) -> StudentData:
+            if msg is None or msg.data is None:
+                return None
+            if msg.type == MessageTypes.STUDENT_GETTING_QUESTIONS:
+                data = msg.data
+                time.sleep(choose_waiting_time)
+                questions = data.questions + (random.randint(0, 999),)
+                data = copy.copy(data)
+                data.questions = questions
+                return data
+            elif msg.type == MessageTypes.STUDENT_ANSWERING:
+                data = msg.data
+                time.sleep(choose_waiting_time)
+                grade = random.choice([2.0, 3.0, 3.5, 4.0, 4.5, 5.0])
+                grades = data.grades + (grade,)
+                data = copy.copy(data)
+                data.grades = grades
+                return data
+            else:
+                return None
+
+        def handle1(msg: Msg):
+            new_data = handle_questions_answers(msg)
+            msg.data = new_data
+            self.member_queues[1].put(msg)
+
+        def handle2(msg: Msg):
+            new_data = handle_questions_answers(msg)
+            msg.data = new_data
+            self.member_queues[2].put(msg)
+
+        def handle_last(msg: Msg):
+            new_data = handle_questions_answers(msg)
+            if msg.type == MessageTypes.STUDENT_GETTING_QUESTIONS:
+                self.thinking_queue.put(
+                    Msg(
+                        MessageTypes.STUDENT,
+                        new_data
+                    )
+                )
+            elif msg.type == MessageTypes.STUDENT_ANSWERING:
+                student = msg.data.student
+                avg = sum(new_data.grades) / 3
+                final_grade = (
+                    2.0 if avg < 2.5 else
+                    3.0 if avg < 3.25 else
+                    3.5 if avg < 3.75 else
+                    4.0 if avg < 4.25 else
+                    4.5 if avg < 4.75 else
+                    5.0
+                )
+                print(f"Student '{student.student_id}' final grade: {final_grade} from exam : {'Theoretical' if self.theoretical else 'Practical'}")
+
+                self.dean_queue.put(
+                    Msg(
+                        MessageTypes.STUDENT_GRADE,
+                        StudentGrade(student, final_grade, self.theoretical)
+                    )
+                )
+                if final_grade != 2.0:
+                    self.output_queue.put(
+                        Msg(
+                            MessageTypes.STUDENT,
+                            StudentData(student)
+                        )
+                    )
+                self.semaphore.release()
+
+        # Tworzenie członków komisji
+        self.members = [
+            CommisionMember(self.member_queues[0], handle1,self.evacuation_event,self.ending_event),
+            CommisionMember(self.member_queues[1], handle2,self.evacuation_event,self.ending_event),
+            CommisionMember(self.member_queues[2], handle_last,self.evacuation_event,self.ending_event),
+        ]
+
+
+
+    def run(self):
+        print(f"{'Theoretical' if self.theoretical else 'Practical'} commission -running")
+        for member in self.members:
+            member.start()
+        for space in self.thinking_spaces:
+            space.start()
+        print(f"{'Theoretical' if self.theoretical else 'Practical'} commission -running")
+        msg = None
+        while True:
+            #print(f" Komisja - running")
+            if self.evacuation_event.is_set():
+                print(f"{'Theoretical' if self.theoretical else 'Practical'} commission received evacuation signal.")
+                break
+            if self.ending_event.is_set():
+                print(f"{'Theoretical' if self.theoretical else 'Practical'} commission received ending signal.")
+                break
+
+        
+            if not msg:
+                try:
+                    msg = self.input_queue.get(timeout=1)
+                except:
+                    continue
+
+            if not self.semaphore.acquire(timeout=1):
+                continue
+
+            self.member_queues[0].put(
+                Msg(
+                    MessageTypes.STUDENT_GETTING_QUESTIONS,
+                    msg.data
+                )
+            )
+            msg = None
+
+        for member in self.members:
+            member.join()
+        for space in self.thinking_spaces:
+            space.join()
+        print(f"{'Theoretical' if self.theoretical else 'Practical'} commission finished.")
+
+
 
 
 def print_dean_results(student_grades):
@@ -473,8 +366,7 @@ class Dean(threading.Thread):
         time.sleep(random.uniform(0, choose_waiting_time))
         self.field = random.randint(1,self.k)
         print(f"Dean sending signal which field has test : {self.field}")
-        # self.choosed_field.put(self.field)
-        broadcast_to_all_students(Msg(MessageTypes.FIELD, self.field))
+        self.choosed_field.put(self.field)
         
 
         count=count_students_with_field(self.students, self.field)
@@ -598,35 +490,28 @@ def main():
     choosed_field=Queue()
 
 
-    # student_manager = StudentManager(students, before_building_queue,evacuation_event,ending_event)
-    # study_field_filter = StudyFieldFilter(before_building_queue,practical_queue,choosed_field,evacuation_event,ending_event)
- 
+    student_manager = StudentManager(students, before_building_queue,evacuation_event,ending_event)
+    study_field_filter = StudyFieldFilter(before_building_queue,practical_queue,choosed_field,evacuation_event,ending_event)
     practical_commision = Commission(semaphore_practical, practical_queue, theoretical_queue, dean_queue,
                                      evacuation_event,ending_event, theoretical=False)
     theoretical_commision = Commission(semaphore_theoretical, theoretical_queue, result_queue, dean_queue,
                                        evacuation_event,ending_event, theoretical=True)
     dean = Dean(students,dean_queue, evacuation_event,ending_event,student_number,student_grades,choosed_field,k)
-    student_threads = [StudentRunner(student, practical_commision, theoretical_commision, evacuation_event) for student in students]
 
 
-    # student_manager.start()
-    for st in student_threads:
-        st.start()
-
+    student_manager.start()
     practical_commision.start()
     theoretical_commision.start()
     dean.start()
-    # study_field_filter.start()
+    study_field_filter.start()
 
     
 
-    # student_manager.join()
+    student_manager.join()
     practical_commision.join()
     theoretical_commision.join()
     dean.join()
-    for st in student_threads:
-        st.join()
-    # study_field_filter.join()
+    study_field_filter.join()
 
 
     print("chosen field", dean.field)
